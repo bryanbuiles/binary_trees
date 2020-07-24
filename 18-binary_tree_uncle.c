55de4331d189ed6e4895c180e76dd41ddd048e22
bryanbuiles <1482@holbertonschool.com>
2020-07-24 13:24:18 -0500

    task 18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,16 +8,14 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (node == NULL || node->parent->parent == NULL)
+	if (node == NULL || node->parent->parent == NULL ||
+		!node->parent)
 		return (NULL);
 
-	if ((node->parent->parent->right != NULL) &&
-		(node->parent->parent->left != NULL))
-	{
-		if (node->parent->parent->right != node->parent)
-			return (node->parent->parent->right);
-		else if (node->parent->parent->left != node->parent)
-			return (node->parent->parent->left);
-	}
+	if (node->parent->parent->right != node->parent)
+		return (node->parent->parent->right);
+	else if (node->parent->parent->left != node->parent)
+		return (node->parent->parent->left);
+
 	return (NULL);
 }
